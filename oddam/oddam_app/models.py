01fd9918192e26232fd7c58647d56a44cc963c75
DominikSobczak <sobczakdominik@gmail.com>
2022-07-26 21:38:54 +0200

    Model Institution: Line 17 amended

--- oddam/oddam_app/models.py
+++ oddam/oddam_app/models.py
@@ -14,4 +14,20 @@ class Institution(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField()
     type = models.IntegerField(choices=TYPES, default=1)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    # TYPES = (
+    #     (1, 'fundacja'),
+    #     (2, 'organizacja pozarządowa'),
+    #     (3, 'zbiórka lokalna')
+    #
+    # )
+    quantity = models.SmallIntegerField()
     categories = ManyToManyField(Category)
+    institution
+    address =
+
+    name = models.CharField(max_length=64)
+    description = models.TextField()
