1fe24acdf30dabed6a0f664c40f00a403bdd2a2e
DominikSobczak <sobczakdominik@gmail.com>
2022-07-26 21:30:07 +0200

    Created model: Institution

--- oddam/oddam_app/models.py
+++ oddam/oddam_app/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
-class Product(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=64)
 
 
+class Institution(models.Model):
+    TYPES = (
+        (1, 'fundacja'),
+        (2, 'organizacja pozarządowa'),
+        (3, 'zbiórka lokalna')
 
+    )
+    name = models.CharField(max_length=64)
+    description = models.TextField
+    type = models.IntegerField(choices=TYPES, default=1)
+    categories = ManyToManyField(Category)
