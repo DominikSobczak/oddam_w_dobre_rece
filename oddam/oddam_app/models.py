7f0943b8c5c4683d7e23df16f300f25d534ad51d
DominikSobczak <sobczakdominik@gmail.com>
2022-07-27 17:30:56 +0200

    Model Donation created

--- oddam/oddam_app/models.py
+++ oddam/oddam_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=64)
@@ -18,16 +20,15 @@ class Institution(models.Model):
 
 
 class Donation(models.Model):
-    # TYPES = (
-    #     (1, 'fundacja'),
-    #     (2, 'organizacja pozarządowa'),
-    #     (3, 'zbiórka lokalna')
-    #
-    # )
-    quantity = models.SmallIntegerField()
-    categories = ManyToManyField(Category)
-    institution
-    address =
+    quantity = models.SmallIntegerField(default=1)
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=64)
+    phone_number = models.SmallIntegerField(max_length=12)
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateTimeField(default=datetime.now())
+    pick_up_time = models.TimeField(default=datetime.time.now)
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, null=True, default=Null)
 
-    name = models.CharField(max_length=64)
-    description = models.TextField()
