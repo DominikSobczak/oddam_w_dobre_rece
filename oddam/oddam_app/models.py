2b80869637cdcfdf65679b4482acfddfe909fcc5
DominikSobczak <sobczakdominik@gmail.com>
2022-07-27 17:36:31 +0200

    Model Donation: line 31 amended - default time removed; line 33 amended

--- oddam/oddam_app/models.py
+++ oddam/oddam_app/models.py
@@ -28,7 +28,7 @@ class Donation(models.Model):
     city = models.CharField(max_length=64)
     zip_code = models.CharField(max_length=6)
     pick_up_date = models.DateTimeField(default=datetime.now())
-    pick_up_time = models.TimeField(default=datetime.time.now)
+    pick_up_time = models.TimeField()
     pick_up_comment = models.TextField()
-    user = models.ForeignKey(User, null=True, default=Null)
+    user = models.ForeignKey(User, null=True, default=None, on_delete=models.CASCADE)
 
